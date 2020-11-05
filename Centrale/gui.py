9c93142c0cc9d9ebc6a1c34712f77ba5d76c6e5c
rob997 <robperdok1@gmail.com>
2020-11-05 15:31:06 +0100

    Gui panes done + button/entry classes

--- Centrale/gui.py
+++ Centrale/gui.py
@@ -54,88 +54,31 @@ tab2 = Tab(tabControl, 'Device 2')
 tabControl.addTabs(tab1, tab2)
 tabControl.pack(expand=1, fill='both')
 
-# Dev1
+
+# Dev1 tab
 dev1Window = WindowPane(tab1, HORIZONTAL)
 dev1Window.pack(fill=BOTH, expand=1)
 pw1 = WindowPane(dev1Window, VERTICAL)
 
 afstandLabel1 = MyLabel(dev1Window, 'AFSTANDAFBEELDING 1')
 temperatuurLabel1 = MyLabel(dev1Window, 'Temperatuur 1')
-lichtLabel1 = MyLabel(dev1Window, 'Licht1')
+lichtLabel1 = MyLabel(dev1Window, 'Licht 1')
 
 dev1Window.addLabel(afstandLabel1, pw1)
 pw1.addLabel(temperatuurLabel1, lichtLabel1)
 
 
-
-#dev2Window = WindowPane(tab2)
-#dev2Window.pack(fill=BOTH, expand=2)
-
-
-
-
-
-
-# Pane 2
-dev2Window = PanedWindow(tab2)
+# Dev2 tab
+dev2Window = WindowPane(tab2, HORIZONTAL)
 dev2Window.pack(fill=BOTH, expand=1)
+pw2 = WindowPane(dev2Window, VERTICAL)
 
-afstandLabel2 = Label(dev2Window, text="AFSTANDAFBEELDING2")
-dev2Window.add(afstandLabel2)
+afstandLabel2 = MyLabel(dev2Window, 'AFSTANDAFBEELDING 2')
+temperatuurLabel2 = MyLabel(dev2Window, 'Temperatuur 2')
+lichtLabel2 = MyLabel(dev2Window, 'Licht 2')
 
-pw2 = PanedWindow(dev2Window, orient=VERTICAL)
-dev2Window.add(pw2)
-
-top2 = Label(pw2, text='Temperatuur2')
-pw2.add(top2)
-
-bottom2 = Label(pw2, text='Licht2')
-pw2.add(bottom2)
-
-
-
-# Widgets tab 1
-
-# Window panes
-# Pane 1
-#dev1Window = PanedWindow(tab1)
-#dev1Window.pack(fill=BOTH, expand=1)
-
-#pw1 = PanedWindow(dev1Window, orient=VERTICAL)
-#dev1Window.add(pw1)
-
-#afstandLabel1 = Label(dev1Window, text='AFSTANDAFBEELDING1')
-#dev1Window.add(afstandLabel1)
-
-#top1 = Label(pw1, text='Temperatuur1')
-#pw1.add(top1)
-
-#bottom1 = Label(pw1, text='Licht1')
-#pw1.add(bottom1)
-
-
-# Pane 2
-dev2Window = PanedWindow(tab2)
-dev2Window.pack(fill=BOTH, expand=1)
-
-afstandLabel2 = Label(dev2Window, text="AFSTANDAFBEELDING2")
-dev2Window.add(afstandLabel2)
-
-pw2 = PanedWindow(dev2Window, orient=VERTICAL)
-dev2Window.add(pw2)
-
-top2 = Label(pw2, text='Temperatuur2')
-pw2.add(top2)
-
-bottom2 = Label(pw2, text='Licht2')
-pw2.add(bottom2)
-
-
-
-
-
-
-
+dev2Window.addLabel(afstandLabel2, pw2)
+pw2.addLabel(temperatuurLabel2, lichtLabel2)
 
 
 mainFrame.mainloop()
