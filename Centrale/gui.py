c66424de9c40a9d25e274b84e1daf10ce519e3d1
rob997 <robperdok1@gmail.com>
2020-11-05 16:44:13 +0100

    gui button

--- Centrale/gui.py
+++ Centrale/gui.py
@@ -7,7 +7,7 @@ class MainFrame(Tk):
     def __init__(self):
         super().__init__()
         self.title('Central Control Unit')
-        self.geometry('500x500')
+        self.geometry('750x750')
 
 
 class TabControl(ttk.Notebook):
@@ -45,8 +45,23 @@ class MyLabel(Label):
         super().__init__(parent, text=text)
 
 
+class MyButton(Button):
+
+    def __init__(self, parent, text):
+        super().__init__(parent, text=text)
+        self.config(height=1, width=50)
+
+
+class MyEntry(Entry):
+
+    def __init__(self, parent, text):
+        super().__init__(parent, text=text)
+
+
+
 # Definitions
 mainFrame = MainFrame()
+
 # Tabs
 tabControl = TabControl(mainFrame)
 tab1 = Tab(tabControl, 'Device 1')
@@ -55,30 +70,43 @@ tabControl.addTabs(tab1, tab2)
 tabControl.pack(expand=1, fill='both')
 
 
-# Dev1 tab
-dev1Window = WindowPane(tab1, HORIZONTAL)
-dev1Window.pack(fill=BOTH, expand=1)
-pw1 = WindowPane(dev1Window, VERTICAL)
+# Dev1 Tab
+dev1Tab = WindowPane(tab1, HORIZONTAL)
+dev1Tab.pack(fill=BOTH, expand=1)
+pw1 = WindowPane(dev1Tab, VERTICAL)
+pw2 = WindowPane(dev1Tab, VERTICAL)
 
-afstandLabel1 = MyLabel(dev1Window, 'AFSTANDAFBEELDING 1')
-temperatuurLabel1 = MyLabel(dev1Window, 'Temperatuur 1')
-lichtLabel1 = MyLabel(dev1Window, 'Licht 1')
+afstandLabel1 = MyLabel(dev1Tab, 'AFSTAND AFBEELDING 1')
+instellingenLabel1 = MyLabel(dev1Tab, 'Instellingen 1')
+temperatuurLabel1 = MyLabel(dev1Tab, 'Temperatuur 1')
+lichtLabel1 = MyLabel(dev1Tab, 'Licht 1')
 
-dev1Window.addLabel(afstandLabel1, pw1)
-pw1.addLabel(temperatuurLabel1, lichtLabel1)
+dev1Tab.addLabel(pw1, pw2)
+pw1.addLabel(afstandLabel1, instellingenLabel1)
+pw2.addLabel(temperatuurLabel1, lichtLabel1)
 
 
-# Dev2 tab
-dev2Window = WindowPane(tab2, HORIZONTAL)
-dev2Window.pack(fill=BOTH, expand=1)
-pw2 = WindowPane(dev2Window, VERTICAL)
+#afstandEntry1 = Entry(dev1Tab)
+#temperatuurEntry1 = Entry(dev1Tab)
 
-afstandLabel2 = MyLabel(dev2Window, 'AFSTANDAFBEELDING 2')
-temperatuurLabel2 = MyLabel(dev2Window, 'Temperatuur 2')
-lichtLabel2 = MyLabel(dev2Window, 'Licht 2')
+btn1 = MyButton(dev1Tab, 'Instellen!')
+pw1.addLabel(btn1)
 
-dev2Window.addLabel(afstandLabel2, pw2)
-pw2.addLabel(temperatuurLabel2, lichtLabel2)
+
+# Dev2 Tab
+dev2Tab = WindowPane(tab2, HORIZONTAL)
+dev2Tab.pack(fill=BOTH, expand=1)
+pw3 = WindowPane(dev2Tab, VERTICAL)
+pw4 = WindowPane(dev2Tab, VERTICAL)
+
+afstandLabel2 = MyLabel(dev2Tab, 'AFSTAND AFBEELDING 2')
+instellingenLabel2 = MyLabel(dev2Tab, 'Instellingen 2')
+temperatuurLabel2 = MyLabel(dev2Tab, 'Temperatuur 2')
+lichtLabel2 = MyLabel(dev2Tab, 'Licht 2')
+
+dev2Tab.addLabel(pw3, pw4)
+pw3.addLabel(afstandLabel2, instellingenLabel2)
+pw4.addLabel(temperatuurLabel2, lichtLabel2)
 
 
 mainFrame.mainloop()
