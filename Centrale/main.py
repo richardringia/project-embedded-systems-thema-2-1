c91973ce8e349b67dcdce9dae89e1c8d9ce6d325
Richard Ringia <richard@blendmerk.nl>
2020-11-09 09:33:56 +0100

    Mainframe creation to main.py

--- Centrale/main.py
+++ Centrale/main.py
@@ -2,8 +2,17 @@ import serial
 
 from device import Device
 import connect
+from gui import *
 
-
-device = connect.get_devices()[0]
+# device = connect.get_devices()[0]
 # while True:
     # print(device.get_temp())
+
+mainFrame = MainFrame()
+tabControl = TabControl(mainFrame)
+tabControl.grid(row=0)
+
+for device in connect.get_devices():
+    main = Main(device.port)
+
+mainFrame.mainloop()
